19326540b24f3daaa6b07bf1919a9737be05f6cd
Javier Ferrer <javsferrer@gmail.com>
2026-05-24 00:01:44 -0300

    fix posthog types

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js'
 
 export const initPostHog = () => {
   if (typeof window !== 'undefined') {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: 'https://app.posthog.com',
       capture_pageview: false,
     })
